b33146f224d4770a780e673357f254c959257ab0
taizan-hokuto <55448286+taizan-hokuto@users.noreply.github.com>
2020-05-05 02:49:28 +0900

    Lint

--- tasktray.py
+++ tasktray.py
@@ -2,13 +2,14 @@ from PyQt5.QtWidgets import QSystemTrayIcon, QMenu, QAction
 from PyQt5 import QtGui
 import settings
 
+
 class TrayWidget(QSystemTrayIcon):
- 
+
     def __init__(self, parent=None):
         super().__init__(parent)
         self.parent = parent
         self.create_actions()
-        self.create_menu()       
+        self.create_menu()
         self.create_icon()
 
     def create_actions(self):
@@ -19,11 +20,11 @@ class TrayWidget(QSystemTrayIcon):
     def create_menu(self):
         self.trayIconMenu = QMenu(self.parent)
         self.trayIconMenu.addSeparator()
-        self.trayIconMenu.addAction(self.quitAction)        
+        self.trayIconMenu.addAction(self.quitAction)
 
     def create_icon(self):
         self.setContextMenu(self.trayIconMenu)
         self.setIcon(QtGui.QIcon(settings.ICON))
-    
+
     def quit(self, force=False):
         print("quit()")
