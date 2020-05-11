fce18321798a2e914ffe12a0602b93d5d3e61291
taizan-hokuto <55448286+taizan-hokuto@users.noreply.github.com>
2020-05-11 23:16:47 +0900

    Remove tr() and modify duplicate lines

--- tasktray.py
+++ tasktray.py
@@ -17,18 +17,18 @@ class TrayWidget(QSystemTrayIcon):
 
         # Acton - Show main widget (menu and double click the tray icon)
         self.showMainWidgetAction = QAction(self)
-        self.showMainWidgetAction.setText(self.tr("Show &Main Widget"))
+        self.showMainWidgetAction.setText("Show &Main Widget")
         self.showMainWidgetAction.setShortcutVisibleInContextMenu(True)
         self.showMainWidgetAction.triggered.connect(self.showMainWidget)
         # Action - Show configulation dialog
         self.showConfigAction = QAction(self)
-        self.showConfigAction.setText(self.tr("&Config"))
+        self.showConfigAction.setText("&Config")
         self.showConfigAction.triggered.connect(self.showConfig)
         # Trigger event when tasktray icon double clicked
         self.activated.connect(self.iconActivated)
         # Action - Quit app
         self.quitAction = QAction(self)
-        self.quitAction.setText(self.tr("&Quit"))
+        self.quitAction.setText("&Quit")
         self.quitAction.triggered.connect(self.quit)
 
     def create_menu(self):
@@ -52,8 +52,7 @@ class TrayWidget(QSystemTrayIcon):
     def showConfig(self, force=False):
         # When the configuration dialog is displayed,
         # the main widget is also displayed at the same time.
-        self.parent.setVisible(True)
-        self.parent.setWindowState(self.parent.windowState() & ~(Qt.WindowMinimized | Qt.WindowActive))
+        self.showMainWidget()
         self.parent.config_dialog()
 
     def iconActivated(self, reason):
